86673b1ff67c2da99151f9ee98f560c146aa964e
Nathan Getachew <nathangetachew95@gmail.com>
2023-03-31 14:05:04 +0300

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Nathan Getachew on 3/31/23.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
